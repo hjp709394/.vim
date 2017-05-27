6259a637aa50c61edf3706085edf16755b559f57
Cucumber <549762085@qq.com>
2017-05-27 10:42:10 -0700

    Add java to tags; rm temporary files from Lookup's tags

--- bin/gftags.sh
+++ bin/gftags.sh
@@ -7,4 +7,4 @@
     #| sort -f >> filenametags
 
 echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > "$2"
-find $1 -type f -not -path '*/.git/*' -printf "%f\t%p\t1\n" | sort -f >> "$2"
+find $1 -type f -not -path '*/.git/*' -not -name '*~' -not -name '*.swp' -not -name '*.swo' -printf "%f\t%p\t1\n" | sort -f >> "$2"
