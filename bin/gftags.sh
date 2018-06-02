d7c2f4b7b2d6e3aba94185ae5b0349664314dcaa
Jiapei Huang <hjp@fb.com>
2018-06-02 10:57:06 -0700

    rm not useful comment

--- bin/gftags.sh
+++ bin/gftags.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 #generate tag file for lookupfile plugin
 #Used by lookupfile plugin of vim
-
-#echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > filenametags
-#find . -type f -printf "%f\t%p\t1\n" \
-    #| sort -f >> filenametags
+#format:
+# file_name\tfile_full_path\t1
 
 echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > "$2"
 find -L $1 -type f -not -path '*/.git/*' -not -name '*.obj' -not -name '*.exe' -not -name '*.dll' -not -name '*.lib' -not -name '*.so' -not -name '*~' -not -name '*.swp' -not -name '*.swo' | sed "s:\(.*\)/\([^/]*\):\2 \1/\2 1:g" | awk '{print $1"\t"$2"\t"$3}' | sort -f >> "$2"
